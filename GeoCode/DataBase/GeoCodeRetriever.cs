f1e203cd4a1cf7cbdfae454d40bf70a737f7c177
max nesler <maxnesler@gmail.com>
2017-05-21 22:35:53 -0400

    first commit, w/o ReSharper, tests are tricky to run :/

---
+++ GeoCode/DataBase/GeoCodeRetriever.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net.Http;
+
+
+namespace GeoCode.DataBase
+{
+    public class GeoCodeRetriever
+    {
+        HttpClient _client;
+        public GeoCodeRetriever()
+        {
+            _client = new HttpClient();
+        }
+        public Models.GeoCode GeoCodeRetrieveAddress(string address)
+        {
+            var geoCode = new Models.GeoCode();
+            if (!string.IsNullOrEmpty(address))
+            {
+                try
+                {
+                    using (_client)
+                    {
+                        var url = $"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key=AIzaSyD8rrhmAGCl28sFl-IwYeTNYBRlJU327YU";
+                        var response = _client.GetAsync(url).Result;
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            //having trouble with ReadAsAsync?
+                            //http://stackoverflow.com/questions/10399324/where-is-httpcontent-readasasync
+                            //install-package Microsoft.AspNet.WebApi.Client
+                            geoCode = response.Content.ReadAsAsync<Models.GeoCode>().Result;
+                        }
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            return geoCode;
+        }
+    }
+}
---
+++ GeoCode/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //read from input file, 
+            try
+            {   // Open the text file using a stream reader.
+                using (StreamReader sr = new StreamReader("TestData/input.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    string line = sr.ReadToEnd();
+                    //parse for /r/n chars
+                    Console.WriteLine(line);
+                }
+            }
+        catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+            //parse into object
+
+            //make call for each address object
+
+
+
+            //AIzaSyD8rrhmAGCl28sFl-IwYeTNYBRlJU327YU
+            //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyD8rrhmAGCl28sFl-IwYeTNYBRlJU327YU
+
+            //on return, 
+
+            //is in box?
+
+            //add to return collection
+
+            //return
+        }
+    }
+}
